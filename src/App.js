808a426120c0dc7dd2e38eac6c6fecb7e5a0ddda
naveendk <naveenkolambage@gmail.com>
2019-03-30 11:20:17 +1300

    added the basic elements , with card

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Row } from "react-bootstrap";
+import SubTotal from "./components/Subtotal/Subtotal";
+import PickupSavings from "./components/PickupSavings/Pickupsavings";
+import TaxesFees from "./components/TaxesFees/TaxesFees";
+import EstimatedTotal from "./components/EstimatedTotal/EstimatedTotal";
+import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: 100,
+      PickupSavings: -3.85,
+      taxes: 0,
+      estimatedTotal: 0
+    };
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container">
+        <Row className="purchase-card">
+          <SubTotal price={this.state.total.toFixed(2)} />
+          <PickupSavings price={this.state.PickupSavings} />
+          <TaxesFees taxes={this.state.taxes.toFixed(2)} />
+          <hr />
+          <EstimatedTotal price={this.state.estimatedTotal.toFixed(2)} />
+        </Row>
       </div>
     );
   }
